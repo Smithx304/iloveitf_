19eb90634a3d29f2592737d4a05492a3db682a85
Smithx304 <smith.spancer@itfgroup.com>
2025-05-19 15:00:33 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
 // app/layout.tsx
+import Navbar from '../components/Navbar';
+import './globals.css';
+
 export const metadata = {
   title: 'iloveITF',
   description: 'Upload your CSV to get the latest paperwork',
